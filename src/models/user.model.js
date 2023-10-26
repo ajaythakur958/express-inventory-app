a8e4fa4cbe543bcd77b064a5d791ac0be72e1b40
Ajay <2ajaythakre@gmail.com>
2023-10-26 21:57:10 +0530

    added register form

---
+++ src/models/user.model.js
@@ -0,0 +1,8 @@
+export default class userModel{
+    constructor(id,name,email,password){
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+}
